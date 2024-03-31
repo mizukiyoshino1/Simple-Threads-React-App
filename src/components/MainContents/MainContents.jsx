ebfb56466a25ffb5b1cd0554706c12bbb8ba9146
mizukiyoshino1 <mizukiyoshino32@gmail.com>
2024-03-31 18:01:29 +0900

    いいね機能の実装

--- src/components/MainContents/MainContents.jsx
+++ src/components/MainContents/MainContents.jsx
@@ -3,10 +3,15 @@ import axios from 'axios';
 import './MainContents.css';
 import Header from '../Header/Header';
 import MessageIcon from './MessageIcon/MessageIcon';
+import { useAuthContext } from '../../context/AuthContext';
 
 function MainContents() {
     // 投稿情報
     const [contents, setContents] = useState([]);
+
+    // ユーザ情報
+    const { user } = useAuthContext();
+    const userId = user.uid;
 
     /**
      * 初期化処理
@@ -15,7 +20,7 @@ function MainContents() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:8080/api/contents');
+                const response = await axios.post('http://localhost:8080/api/contents',{userId:userId});
                 setContents(response.data); // データを設定する
             } catch (error) {
                 console.error('Error fetching data:', error);
--- src/components/MainContents/MessageIcon/MessageIcon.jsx
+++ src/components/MainContents/MessageIcon/MessageIcon.jsx
@@ -10,6 +10,23 @@ const MessageIcon = ({ content }) => {
     const userId = user.uid;
 
     /**
+     * いいね処理
+     * 
+     */
+    const handleLike = async () => {
+        try {
+            const response = await axios.post('http://localhost:8080/api/like', {
+                userId: userId,
+                reportId: content.id,
+                likeFlag: 1
+            })
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error deleting post:', error)
+        }
+    }
+
+    /**
      * 投稿の削除機能
      * 
      */
@@ -42,9 +59,21 @@ const MessageIcon = ({ content }) => {
                     <div className="content-text"><span>{content.content}</span></div>
                 </div>
             </div>
-            {userId === content.userId &&
-                <button type="button" onClick={handleDelete}>Delete</button>
-            }
+            <div className="border-area">
+                <button type="button" onClick={handleLike} className="like-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                    </svg>
+                </button>
+                {content.likeCount !== 0 && <p className="like-count">{content.likeCount}</p>}
+                {userId === content.userId && (
+                    <button type="button" onClick={handleDelete} className="delete-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                        </svg>
+                    </button>
+                )}
+            </div>
         </div>
     );
 }
