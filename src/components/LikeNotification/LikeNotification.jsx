43f614c4471ec225e1f093550fb43fc0facaab5e
mizukiyoshino1 <mizukiyoshino32@gmail.com>
2024-04-13 16:54:10 +0900

    いいね通知機能の追加

--- src/components/LikeNotification/LikeNotification.jsx
+++ src/components/LikeNotification/LikeNotification.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './LikeNotification.css';
 import Header from '../Header/Header';
+import { useAuthContext } from '../../context/AuthContext';
+import defaultImage from '../../assets/img/default-profile-Img.png';
 
 const LikeNotification = () => {
+
+    // ユーザ情報
+    const { user } = useAuthContext();
+    const userId = user.uid;
+
+    // 通知情報
+    const [notifications, setNotifications] = useState([]);
+
+    /**
+     * 初期化処理
+     * 
+     */
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // レポート取得
+                const respReports = await axios.post('http://localhost:8080/api/notifications',{userId:userId});
+                setNotifications(respReports.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return(
         <>
             <Header/>
             <div className="like-notification">
-                <h2>Like +1</h2>
-                <h2>Like +2</h2>
-                <h2>Like +3</h2>
+                {notifications.map(notification => (
+                    <div key={notification.reportId} className="notification-item">
+                        {notification.profileImageUrl === '' ?
+                            <img
+                                src={defaultImage}
+                                alt="Default Profile"
+                                className="users-profile-Img"
+                            /> :
+                            <img
+                                src={notification.profileImageUrl}
+                                alt="Profile"
+                                className="users-profile-Img"
+                            />
+                        }
+                        <div>
+                            <h3>{notification.userName} liked your report</h3>
+                            <p>
+                                {notification.content.length > 20 ?
+                                    notification.content.slice(0, 20) + '...' :
+                                    notification.content}
+                            </p>
+
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     )
